03ade25f5c302a1e793a588442167fa40c8f1786
AnushkaC2008 <79006208+AnushkaC2008@users.noreply.github.com>
2021-06-30 15:52:14 +0530

    Add files via upload

---
+++ Fire.js
@@ -0,0 +1,13 @@
+class Fire {
+    constructor(posX) {
+     
+      this.rx = posX; //setting the x posing where wall will be created  
+      this.ry = height-random([300, 200, 450]);   //setting y position where wall will be created 
+      this.spt=createSprite(this.rx, this.ry); //using rx,ry
+      this.spt.shapeColor="green";
+      //this.spt.addAnimation("ground",wallAnimation);
+      //this.spt.scale=0.10;
+    
+    }
+  
+}
